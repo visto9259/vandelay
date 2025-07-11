c44387d9611ca38345a49ef4f9e3220d60c30e04
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-07-11 16:42:25 -0400

    Removed commented code
    Signed-off-by: Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>

--- src/Chorus/src/Service/AbstractService.php
+++ src/Chorus/src/Service/AbstractService.php
@@ -12,25 +12,8 @@ use GuzzleHttp\Exception\GuzzleException;
 use Psr\Cache\InvalidArgumentException;
 
 use function array_map;
-/*
-use function count;
-use function curl_close;
-use function curl_error;
-use function curl_exec;
-use function curl_init;
-use function curl_setopt;
-use function explode;
-use function http_build_query;
-*/
 use function implode;
 use function json_decode;
-
-/*
-use const CURLOPT_HEADER;
-use const CURLOPT_HTTPHEADER;
-use const CURLOPT_RETURNTRANSFER;
-use const CURLOPT_URL;
-*/
 
 class AbstractService
 {
@@ -65,33 +48,5 @@ class AbstractService
             return implode(', ', $values);
         }, $response->getHeaders());
         return json_decode($response->getBody()->getContents(), true);
-/*
-        $ch = curl_init();
-        if (! $ch) {
-            throw new Exception('Cannot create curl handle');
-        }
-        $url .= count($queryParams) ? '?' . http_build_query($queryParams) : '';
-        curl_setopt($ch, CURLOPT_URL, $this->options->getBaseUrl() . $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        $response = curl_exec($ch);
-        if (false === $response) {
-            $error = curl_error($ch);
-            throw new Exception($error);
-        }
-        [$header, $body] = explode("\r\n\r\n", $response, 2);
-
-        $responseHeaders = [];
-
-        foreach (explode("\r\n", $header) as $headerItem) {
-            $a = explode(': ', $headerItem, 2);
-            if (count($a) === 2) {
-                $responseHeaders[$a[0]] = $a[1];
-            }
-        }
-        curl_close($ch);
-        return json_decode($body, true);
-*/
     }
 }
