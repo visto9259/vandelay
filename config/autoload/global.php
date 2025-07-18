ae00ce7ddb38f6fd4f59c5ddafa293f07a126938
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-07-17 21:39:13 -0400

    Added application id to chorus api config
    Signed-off-by: Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 return [
     'chorus_api'   => [
-        'client_id'     => getenv('CLIENT_ID') ?? '',
-        'client_secret' => getenv('CLIENT_SECRET'),
-        'base_url'      => getenv('BASE_URL'),
-        'token_url'     => getenv('TOKEN_URL'),
-        'scope'         => getenv('SCOPE'),
-        'callback_url'  => getenv('CALLBACK_URL'),
+        'client_id'      => getenv('CLIENT_ID'),
+        'client_secret'  => getenv('CLIENT_SECRET'),
+        'base_url'       => getenv('BASE_URL'),
+        'token_url'      => getenv('TOKEN_URL'),
+        'scope'          => getenv('SCOPE'),
+        'callback_url'   => getenv('CALLBACK_URL'),
+        'application_id' => getenv('APPLICATION_ID'),
     ],
     'proxy_config' => [
         'events_file' => __DIR__ . '/../../data/events/events.log',
