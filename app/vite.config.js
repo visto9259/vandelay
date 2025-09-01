1962fc3f4fba61fa2599bc4222578af73514208a
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-09-01 12:23:28 -0400

    Added Vite plugin for version
    Signed-off-by: Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {vitePluginVersionMark} from "vite-plugin-version-mark";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: [
-          'src/main.jsx',
-      ],
-    },
-    outDir: '../public/dist',
-  }
+export default defineConfig(({mode}) => {
+    return {
+        plugins: [
+            react(),
+            vitePluginVersionMark({
+                ifGlobal: true,
+                command: mode === 'development' ? "git branch --show-current" : "git describe --tags --abbrev=0"
+            })
+        ],
+        build: {
+            manifest: true,
+            rollupOptions: {
+                input: [
+                    'src/main.jsx',
+                ],
+            },
+            outDir: '../public/dist',
+        }
+    }
 })
