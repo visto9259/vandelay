eba3ec714de2f9f4a5de90384399f31d0ee33574
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-09-02 13:57:49 -0400

    Added isLoading to state
    Signed-off-by: Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>

--- app/src/store/applicationSlice.js
+++ app/src/store/applicationSlice.js
@@ -6,16 +6,38 @@ const applicationService = new ApplicationService();
 export const ApplicationSlice = createSlice({
   name: "ApplicationSlice",
   initialState: {
+    isLoading: false,
     loaded: false,
     applications: [],
   },
   reducers: {
+    setIsLoading: (state, action) => {
+        state.isLoading = action.payload;
+    },
     setLoaded: (state, action) => {
       state.loaded = action.payload;
     },
     setApplications: (state, action) => {
       state.applications = action.payload;
     },
+    updateInstallation: (state, action) => {
+        const updatedInstallation = action.payload;
+
+        state.applications = state.applications.map((application) => {
+            if (application.id !== updatedInstallation.appId) {
+                return application;
+            }
+            return  {
+                ...application,
+                installations: application.installations.map((installation) => {
+                    if (installation.id !== updatedInstallation.id) {
+                        return {...installation};
+                    }
+                    return updatedInstallation;
+                }),
+            };
+        });
+    }
   }
 });
 
@@ -27,6 +49,7 @@ export const ApplicationSlice = createSlice({
 export const getApplications = (applicationId) => {
   return (dispatch) => {
     dispatch(setLoaded(false));
+    dispatch(setIsLoading(true));
     const apps = [];
     const promises = [];
     promises.push(applicationService.getApplications(applicationId).then(applications => {
@@ -47,6 +70,7 @@ export const getApplications = (applicationId) => {
     return Promise.all(promises).then(() => {
       dispatch(setApplications(apps));
       dispatch(setLoaded(true));
+      dispatch(setIsLoading(false));
     })
     /*
     return applicationService.getApplications().then(applications => {
@@ -61,5 +85,7 @@ export const getApplications = (applicationId) => {
 export const {
   setLoaded,
   setApplications,
+  updateInstallation,
+  setIsLoading,
 } = ApplicationSlice.actions;
 export default ApplicationSlice.reducer;
