ab69a60e9b2a55a97e8512cc897bdd379d6e878a
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-09-16 17:20:45 -0400

    Updated Home page
    Signed-off-by: Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>

--- app/src/Home.jsx
+++ app/src/Home.jsx
@@ -1,4 +1,5 @@
 import {Col, Row} from "react-bootstrap";
+import React from "react";
 
 const Home = () => {
 
@@ -7,6 +8,8 @@ const Home = () => {
         <Row>
           <Col>
             <h2>Welcome to the Vandelay Flex Energy Demo</h2>
+              <p>The Vandelay Energy is a mock Commercial Aggregator that demonstrates the capabilities of Chorus Transact.</p>
+              <p>Version {__VANDELAY_APP_VERSION__}</p>
           </Col>
         </Row>
     </>
