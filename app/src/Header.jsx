dc4fc708c2f3ceea1c3644fd6ef91a77b93eda9a
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-09-22 09:29:15 -0400

    Moved nav under nonav condition check
    Signed-off-by: Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>

--- app/src/Header.jsx
+++ app/src/Header.jsx
@@ -15,18 +15,20 @@ function Header({nonav = false}) {
               <img alt="" src="/dist/vandelay.svg" width="30" height="30" className="d-inline-block align-top"/>
               {' '}Vandelay
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             {!nonav && (
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <NavLink to="/applications" className={'nav-link'}>DER Program</NavLink>
-                  <NavLink to="/devices" className={'nav-link'}>Systems</NavLink>
-                  <NavLink to="/monitor" className={'nav-link'}>Monitor</NavLink>
-                </Nav>
-                <Nav>
-                  <NavLink to="#" className="nav-link" onClick={()=>setShowAboutModal(true)}>About</NavLink>
-                </Nav>
-              </Navbar.Collapse>
+                <>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavLink to="/applications" className={'nav-link'}>DER Program</NavLink>
+                            <NavLink to="/devices" className={'nav-link'}>Systems</NavLink>
+                            <NavLink to="/monitor" className={'nav-link'}>Monitor</NavLink>
+                        </Nav>
+                        <Nav>
+                            <NavLink to="#" className="nav-link" onClick={()=>setShowAboutModal(true)}>About</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </>
             )}
           </Container>
         </Navbar>
